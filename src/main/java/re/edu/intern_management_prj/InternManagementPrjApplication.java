e5b8e633d87eea954f4ccdda1b6bc2f520ecc7bd
SaikiroMurphy <ducanhn2810@gmail.com>
2026-05-05 20:12:33 +0700

    first commit

---
+++ src/main/java/re/edu/intern_management_prj/InternManagementPrjApplication.java
@@ -0,0 +1,13 @@
+package re.edu.intern_management_prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternManagementPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternManagementPrjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/re/edu/intern_management_prj/InternManagementPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package re.edu.intern_management_prj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternManagementPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
